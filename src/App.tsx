acfa88a11e98a224a1ae6fde66656abb05df4d8d
Ryan Nielson <ryan.nielson@gmail.com>
2019-12-04 17:36:45 -0400

    Added top bar to the ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import CssBaseline from './components/CssBaseline';
 import styled from 'styled-components';
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Base64 } from 'js-base64';
 
-const Wrapper = styled.div`
+const Wrapper = styled.div.attrs({ spellcheck: false })`
   position: absolute;
-  top: 0;
+  top: 2em;
   left: 0;
   right: 0;
   bottom: 0;
@@ -23,6 +24,8 @@ const TextArea = styled.textarea`
   color: transparent;
   caret-color: rgba(0, 0, 0, 1);
   line-height: 1.5;
+  border: 0;
+  outline: none;
 `;
 
 const HighlightedText = styled(SyntaxHighlighter)`
@@ -33,12 +36,71 @@ const HighlightedText = styled(SyntaxHighlighter)`
   line-height: 1.5;
 `;
 
+const Bar = styled.div`
+  color: #ffffff;
+  padding: 0 0.5em;
+  height: 2em;
+  vertical-align: middle;
+  line-height: 2em;
+  background-color: #718096;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const UrlArea = styled.span`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const CopyUrlButton = styled.button`
+  appearance: none;
+  background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+  text-decoration: none;
+  outline: none;
+  padding: 0;
+`;
+
+const GeneratedUrl = styled.input.attrs({ type: 'text', spellcheck: false, readOnly: true })`
+  appearance: none;
+  background: none;
+  border: none;
+  text-decoration: none;
+  outline: none;
+  flex: 1;
+  text-align: right;
+  padding: 0 0.5em;
+  color: #ffffff;
+  max-width: 100%;
+`;
+
 const App: React.FC = () => {
   const [code, setCode] = useState("puts 'Hello, world!'");
+  const [url, setUrl] = useState('');
+  // const urlInputRef = useRef(null);
+
+  useEffect(() => {
+    console.log(window.location);
+    setUrl(`${window.location.origin}/${Base64.encodeURI(code)}`);
+  }, [code]);
+
+  // const copyUrlToClipboard = e => {
+  //   urlInputRef.current.select();
+  // };
 
   return (
     <>
       <CssBaseline />
+      <Bar>
+        <span>pastehaste</span>
+        <UrlArea>
+          <GeneratedUrl value={url} />
+          <CopyUrlButton>copy url</CopyUrlButton>
+        </UrlArea>
+      </Bar>
       <Wrapper>
         <TextArea value={code} onChange={(e): void => setCode(e.currentTarget.value)} />
         <HighlightedText language="ruby">{code}</HighlightedText>
