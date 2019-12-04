9c0a245d59a51d56375d15595ce6f0f803ba5787
Ryan Nielson <ryan.nielson@gmail.com>
2019-12-04 16:44:30 -0400

    Started working on editor. Basics completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CssBaseline from './components/CssBaseline';
+import styled from 'styled-components';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  resize: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 0.5em;
+  background: transparent;
+  color: transparent;
+  caret-color: rgba(0, 0, 0, 1);
+  line-height: 1.5;
+`;
+
+const HighlightedText = styled(SyntaxHighlighter)`
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  padding: 0.5em;
+  line-height: 1.5;
+`;
 
 const App: React.FC = () => {
+  const [code, setCode] = useState("puts 'Hello, world!'");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Wrapper>
+        <TextArea value={code} onChange={(e): void => setCode(e.currentTarget.value)} />
+        <HighlightedText language="ruby">{code}</HighlightedText>
+      </Wrapper>
+    </>
   );
 };
 
